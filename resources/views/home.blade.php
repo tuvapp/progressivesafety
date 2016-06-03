efc25eb13941e71ee1b22862ae6e6b56423f6f89
Rikard Olsson <rikard@thryselius.se>
2016-06-03 10:26:01 +0200

    First commit, before deploying

---
+++ resources/views/home.blade.php
@@ -0,0 +1,228 @@
+@extends('master')
+
+@section('content')
+    <section class="no-mb">
+            <!-- *** JUMBOTRON ***
+ _________________________________________________________ -->
+
+            <div class="jumbotron">
+
+                <div class="dark-mask"></div>
+
+                <div class="container">
+                    <div class="row mb-small">
+                        <div class="col-sm-12 text-center" style="background:rgba(0,0,0,0.1);">
+                            <h1>Brandsläckning för <u>alla</u></h1>
+                        </div>
+                    </div>
+
+                    <!-- <div class="row">
+
+                        <div class="col-sm-6 text-center-xs">
+                            <p class="text-uppercase">Business. Corporate. Agency.
+                                <br />Portfolio. Blog. E-commerce.
+                                <br />We have covered everything.</p>
+                            <p>See our current packages comparison
+                                <br />to choose the right one for you.</p>
+                            <p><a href="#packages" class="scroll-to btn btn-template-transparent-black">See packages</a>
+                            </p>
+
+                        </div>
+                    </div> -->
+                </div>
+            </div>
+
+            <!-- *** JUMBOTRON END *** -->
+        </section>
+
+        <section class="bar no-mb color-white" style="background:#e65852;">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12">
+                        <h1>Vår affärsidé</h1>
+
+                        <p class="lead">
+                            Progressive Safety är ett svenskt bolag som utvecklar livsavgörande hjälpmedel inom området säkerhet.
+                        </p>
+
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section class="bar background-pentagon no-mb">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="heading text-center">
+                            <h2>Referenser</h2>
+                        </div>
+                        <center>
+                            <p class="lead">
+                                Vi har arbetat med många nöjda kunder och vi vill mer än gärna presentera några av dem med citat.
+                            </p>
+                        </center>
+
+
+                        <!-- *** TESTIMONIALS CAROUSEL ***
+ _________________________________________________________ -->
+                    <center>
+                        <ul class="owl-carousel testimonials same-height-row">
+                            <li class="item">
+                                <div class="testimonial same-height-always">
+                                    <div class="text">
+                                        <p>Jag tycker att det är jättebra, eller kanske tillochmed svinbra som vår programmerare Rikard hade uttryckt det. Vettiga lösningar, lämnar bra garantier och håller alltid vad de lovar. Vad som kan komma att väntas av detta bolaget med deras idéer och nytänk kan vi bara fantisera om.</p>
+                                    </div>
+                                    <div class="bottom">
+                                        <div class="icon"><i class="fa fa-quote-left"></i>
+                                        </div>
+                                        <div class="name-picture">
+                                            <img class="" alt="" src="img/references/emil_karlsson.jpg">
+                                            <h5>Emil Karlsson</h5>
+                                            <p>CEO, <a href="www.tuva.co">Tuva</a></p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </li>
+                            <li class="item">
+                                <div class="testimonial same-height-always">
+                                    <div class="text">
+                                        <p>Jag tycker att det är jättebra, eller kanske tillochmed svinbra som vår programmerare Rikard hade uttryckt det. Vettiga lösningar, lämnar bra garantier och håller alltid vad de lovar. Vad som kan komma att väntas av detta bolaget med deras idéer och nytänk kan vi bara fantisera om.</p>
+                                    </div>
+                                    <div class="bottom">
+                                        <div class="icon"><i class="fa fa-quote-left"></i>
+                                        </div>
+                                        <div class="name-picture">
+                                            <img class="" alt="" src="img/references/carlos_martin.JPG">
+                                            <h5>Carlos M. Martin</h5>
+                                            <p>CEO, <a href="www.tuva.co">Tuva</a></p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </li>
+                            <li class="item">
+                                <div class="testimonial same-height-always">
+                                    <div class="bottom">
+                                        <div class="icon"><i class="fa fa-quote-left"></i>
+                                        </div>
+                                        <div class="name-picture">
+                                            <img class="" alt="" src="img/references/mattias_karlsson.jpg">
+                                            <h5>Mattias Karlsson</h5>
+                                            <p>CEO, <a href="www.tuva.co">Tuva</a></p>
+                                        </div>
+                                    </div>
+                                    <div class="text">
+                                        <p>
+                                            Jag tycker att det är jättebra, eller kanske tillochmed svinbra som vår programmerare Rikard hade uttryckt det. Vettiga lösningar, lämnar bra garantier och håller alltid vad de lovar. Vad som kan komma att väntas av detta bolaget med deras idéer och nytänk kan vi bara fantisera om.
+                                        </p>
+                                    </div>
+                                </div>
+                            </li>
+                            <li class="item">
+                                <div class="testimonial same-height-always">
+                                    <div class="text">
+                                        <p>Jag tycker att det är jättebra, eller kanske tillochmed svinbra som vår programmerare Rikard hade uttryckt det. Vettiga lösningar, lämnar bra garantier och håller alltid vad de lovar. Vad som kan komma att väntas av detta bolaget med deras idéer och nytänk kan vi bara fantisera om.</p>
+                                    </div>
+
+                                    <div class="bottom">
+                                        <div class="icon"><i class="fa fa-quote-left"></i>
+                                        </div>
+                                        <div class="name-picture">
+                                            <img class="" alt="" src="img/person-4.jpg">
+                                            <h5>Ricke Rik</h5>
+                                            <p>CEO, <a href="#">Microsoft</a></p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </li>
+                            <!--<li class="item">
+                                <div class="testimonial same-height-always">
+                                    <div class="text">
+                                        <p>It showed a lady fitted out with a fur hat and fur boa who sat upright, raising a heavy fur muff that covered the whole of her lower arm towards the viewer. Gregor then turned to look out the window at the dull
+                                            weather. Drops of rain could be heard hitting the pane, which made him feel quite sad. Gregor then turned to look out the window at the dull weather. Drops of rain could be heard hitting the pane, which made
+                                            him feel quite sad.</p>
+                                    </div>
+
+                                    <div class="bottom">
+                                        <div class="icon"><i class="fa fa-quote-left"></i>
+                                        </div>
+                                        <div class="name-picture">
+                                            <img class="" alt="" src="img/person-4.jpg">
+                                            <h5>John McIntyre</h5>
+                                            <p>CEO, TransTech</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </li> -->
+                        </ul>
+                    </center>
+                        <!-- /.owl-carousel -->
+
+                        <!-- *** TESTIMONIALS CAROUSEL END *** -->
+                    </div>
+
+                </div>
+            </div>
+        </section>
+        <!-- /.bar -->
+
+        <section class="bar background-image-fixed-2 no-mb color-white text-center">
+            <div class="dark-mask"></div>
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12">
+                        <h3 class="text-uppercase">Vill du veta mer?</h3>
+                        <p class="lead">Vi har skapat en instruktionsvideo för att demonstrera en av våra smarta produkter.</p>
+                        <p class="text-center">
+                            <a href="/how-it-works" class="btn btn-template-transparent-black btn-lg">Så funkar det</a>
+                        </p>
+                    </div>
+
+                </div>
+            </div>
+        </section>
+
+        </section>
+        <!-- /.bar -->
+
+        <section class="bar background-gray no-mb">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="heading text-center">
+                            <h2>Några av våra kunder</h2>
+                        </div>
+
+                        <ul class="owl-carousel customers">
+                            <li class="item">
+                                <img src="img/customer-1.png" alt="" class="img-responsive">
+                            </li>
+                            <li class="item">
+                                <img src="img/customer-2.png" alt="" class="img-responsive">
+                            </li>
+                            <li class="item">
+                                <img src="img/customer-3.png" alt="" class="img-responsive">
+                            </li>
+                            <li class="item">
+                                <img src="img/customer-4.png" alt="" class="img-responsive">
+                            </li>
+                            <li class="item">
+                                <img src="img/customer-5.png" alt="" class="img-responsive">
+                            </li>
+                            <li class="item">
+                                <img src="img/customer-6.png" alt="" class="img-responsive">
+                            </li>
+                        </ul>
+                        <!-- /.owl-carousel -->
+                    </div>
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div class="see-more">
+                                <a href="/customers" class="btn btn-template-main">Se fler Kunder här</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+@endsection
---
+++ resources/views/master.blade.php
@@ -0,0 +1,295 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="robots" content="all,follow">
+    <meta name="googlebot" content="index,follow,snippet,archive">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Progressive Safety</title>
+
+    <meta name="keywords" content="">
+
+    <script type="text/javascript" src="js/pace.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="css/pace.css">
+
+    <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,300' rel='stylesheet' type='text/css'>
+    <!-- Bootstrap and Font Awesome css -->
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css">
+
+    <!-- Css animations  -->
+    <link href="css/animate.css" rel="stylesheet">
+
+    <!-- Theme stylesheet, if possible do not edit this stylesheet -->
+    <link href="css/style.red_edit.css" rel="stylesheet" id="theme-stylesheet">
+
+    <!-- Custom stylesheet - for your changes -->
+    <link href="css/custom.css" rel="stylesheet">
+
+    <!-- Responsivity for older IE -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+        <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+<![endif]-->
+
+    <!-- Favicon and apple touch icons-->
+    <link rel="shortcut icon" href="img/icon.ico" type="image/x-icon" />
+    <link rel="apple-touch-icon" href="img/apple-touch-icon.png" />
+    <link rel="apple-touch-icon" sizes="57x57" href="img/apple-touch-icon-57x57.png" />
+    <link rel="apple-touch-icon" sizes="72x72" href="img/apple-touch-icon-72x72.png" />
+    <link rel="apple-touch-icon" sizes="76x76" href="img/apple-touch-icon-76x76.png" />
+    <link rel="apple-touch-icon" sizes="114x114" href="img/apple-touch-icon-114x114.png" />
+    <link rel="apple-touch-icon" sizes="120x120" href="img/apple-touch-icon-120x120.png" />
+    <link rel="apple-touch-icon" sizes="144x144" href="img/apple-touch-icon-144x144.png" />
+    <link rel="apple-touch-icon" sizes="152x152" href="img/apple-touch-icon-152x152.png" />
+    <!-- owl carousel css -->
+
+    <link href="css/owl.carousel.css" rel="stylesheet">
+    <link href="css/owl.theme.css" rel="stylesheet">
+</head>
+
+<body>
+
+    <div id="all" class="scrollbar-light">
+
+        <header>
+
+            <!-- *** TOP ***
+            <div id="top">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-xs-5 contact">
+                            <p class="hidden-sm hidden-xs">Contact us on +420 777 555 333 or hello@universal.com.</p>
+                            <p class="hidden-md hidden-lg"><a href="#" data-animate-hover="pulse"><i class="fa fa-phone"></i></a>  <a href="#" data-animate-hover="pulse"><i class="fa fa-envelope"></i></a>
+                            </p>
+                        </div>
+                        <div class="col-xs-7">
+                            <div class="social">
+                                <a href="#" class="external facebook" data-animate-hover="pulse"><i class="fa fa-facebook"></i></a>
+                                <a href="#" class="external gplus" data-animate-hover="pulse"><i class="fa fa-google-plus"></i></a>
+                                <a href="#" class="external twitter" data-animate-hover="pulse"><i class="fa fa-twitter"></i></a>
+                                <a href="#" class="email" data-animate-hover="pulse"><i class="fa fa-envelope"></i></a>
+                            </div>
+
+                            <div class="login">
+                                <a href="#" data-toggle="modal" data-target="#login-modal"><i class="fa fa-sign-in"></i> <span class="hidden-xs text-uppercase">Sign in</span></a>
+                                <a href="customer-register.html"><i class="fa fa-user"></i> <span class="hidden-xs text-uppercase">Sign up</span></a>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+-->
+            <!-- *** TOP END *** -->
+
+            <!-- *** NAVBAR ***
+    _________________________________________________________ -->
+
+            <div class="navbar-affixed-top" data-spy="affix" data-offset-top="200">
+
+                <div class="navbar navbar-default yamm" role="navigation" id="navbar">
+
+                    <div class="container">
+                        <div class="navbar-header">
+                            <a class="navbar-brand home" href="index.html">
+                                <img src="img/logo@200px.png" alt="Universal logo" class="hidden-xs hidden-sm">
+                                <img src="img/logo@150px.png" alt="Universal logo" class="visible-xs visible-sm"><span class="sr-only">Universal - go to homepage</span>
+                                
+                            </a>
+                            <div class="navbar-buttons">
+                                <button type="button" class="navbar-toggle btn-template-main" data-toggle="collapse" data-target="#navigation">
+                                    <span class="sr-only">Toggle navigation</span>
+                                    <i class="fa fa-align-justify"></i>
+                                </button>
+                            </div>
+                        </div>
+                        <!--/.navbar-header -->
+
+                        <div class="navbar-collapse collapse" id="navigation">
+
+                            <ul class="nav navbar-nav navbar-right">
+                                <li class="nav-item {{ Request::is('/') ? 'active' : '' }}">
+                                  <a class="nav-link" href="/">Hem</a>
+                                </li>
+                                <li class="nav-item {{ Request::is('products*') ? 'active' : '' }}">
+                                  <a class="nav-link" href="/products">Produkter</a>
+                                </li>
+                                <li class="nav-item {{ Request::is('how-it-works') ? 'active' : '' }}">
+                                  <a class="nav-link" href="/how-it-works">Så funkar det</a>
+                                </li>
+                                <li class="nav-item {{ Request::is('customers') ? 'active' : '' }}">
+                                  <a class="nav-link" href="/customers">Kunder</a>
+                                </li>
+                                <li class="nav-item {{ Request::is('contact') ? 'active' : '' }}">
+                                    <a class="nav-link" href="/contact">Kontakt</a>
+                                </li>
+                                <li class="nav-item {{ Request::is('order') ? 'active' : '' }}" style="background:#ed1b12;">
+                                  <a class="nav-link" href="/order" style="color:white;">Beställ</a>
+                                </li>
+                            </ul>
+
+                        </div>
+                        <!--/.nav-collapse -->
+                    </div>
+
+
+                </div>
+                <!-- /#navbar -->
+
+            </div>
+
+            <!-- *** NAVBAR END *** -->
+
+        </header>
+
+        @if (!Request::is('/'))
+        <!-- Is only working right now with one level navigation. -->
+                <div id="heading-breadcrumbs">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-md-7">
+                                <h1>{{ App\Classes\PSTools::urltostring() }}</h1>
+                            </div>
+                            <div class="col-md-5">
+                                <ul class="breadcrumb">
+                                    <li><a href="/">Hem</a>
+                                    </li>
+                                    <li>
+                                        {{ App\Classes\PSTools::urltostring() }}
+                                    </li>
+                                </ul>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+        @endif
+
+
+        @yield('content')
+
+
+        <!-- *** GET IT ***
+
+        <div id="get-it">
+            <div class="container">
+                <div class="col-md-8 col-sm-12">
+                    <h3>Do you want cool website like this one?</h3>
+                </div>
+                <div class="col-md-4 col-sm-12">
+                    <a href="#" class="btn btn-template-transparent-primary">Buy this template now</a>
+                </div>
+            </div>
+        </div>
+
+_________________________________________________________ -->
+        <!-- *** GET IT END *** -->
+
+
+        <!-- *** FOOTER ***
+_________________________________________________________ -->
+
+        <footer id="footer">
+            <div class="container">
+                <div class="col-md-3 col-sm-6">
+                    <h4>Affärsidé</h4>
+                    <p>Progressive Safety är ett svenskt bolag som utvecklar livsavgörande hjälpmedel inom området säkerhet.</p>
+
+                    <hr class="hidden-md hidden-lg hidden-sm">
+
+                </div>
+                <!-- /.col-md-3 -->
+
+                <div class="col-md-3 col-sm-6">
+
+                    <h5><a href="/company" class="inverse">Företaget</a></h5>
+                    <h5><a href="/team" class="inverse">Team</a></h5>
+                    <h5><a href="/our-story" class="inverse">Vår Story</a></h5>
+                    <h5><a href="/media" class="inverse">Press</a></h5>
+
+                </div>
+
+                <!-- /.col-md-3 -->
+
+                <div class="col-md-3 col-sm-6">
+
+                    <h4 class="inverse-red">Återförsäljare</h4>
+                    <h5><a href="http://www.presto.se" class="inverse">Presto Brandsäkerhet AB</a></h5>
+                    <h5><a href="http://www.dafo.se" class="inverse">Dafo Brand</a></h5>
+                    <h5><a href="http://www.kidde.se" class="inverse">Kidde Sweden AB</a></h5>
+                    <h5><a href="/resellers" class="inverse">Mer...</a></h5>
+
+                </div>
+
+                <!-- /.col-md-3 -->
+
+                <div class="col-md-3 col-sm-6">
+
+                    <h4>Kontakt</h4>
+
+                    <p><strong>Progressive Safety</strong>
+                        <br>Gjuterigatan 9
+                        <br>553 18 Jönköping
+                    </p>
+
+                    <a href="/contact" class="btn btn-small btn-template-main">Gå till kontaktsida</a>
+
+                    <hr class="hidden-md hidden-lg hidden-sm">
+
+                </div>
+
+            </div>
+            <!-- /.container -->
+        </footer>
+        <!-- /#footer -->
+
+        <!-- *** FOOTER END *** -->
+
+        <!-- *** COPYRIGHT ***
+_________________________________________________________ -->
+
+        <div id="copyright">
+            <div class="container">
+                <div class="col-md-12">
+                    <p class="pull-left">&copy; 2016. Progressive Safety Sweden AB</p>
+                    </p>
+
+                </div>
+            </div>
+        </div>
+        <!-- /#copyright -->
+
+        <!-- *** COPYRIGHT END *** -->
+
+
+
+    </div>
+    <!-- /#all -->
+
+    <!-- #### JAVASCRIPT FILES ### -->
+
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+    <script>
+        window.jQuery || document.write('<script src="js/jquery-1.11.0.min.js"><\/script>')
+    </script>
+    <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
+
+    <script src="js/jquery.cookie.js"></script>
+    <script src="js/waypoints.min.js"></script>
+    <script src="js/jquery.counterup.min.js"></script>
+    <script src="js/jquery.parallax-1.1.3.js"></script>
+    <script src="js/front.js"></script>
+
+    <!-- owl carousel -->
+    <script src="js/owl.carousel.min.js"></script>
+    
+
+    @yield('script')
+
+
+</body>
+
+</html>
